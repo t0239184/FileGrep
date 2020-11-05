04b911487aecb1df386f1f3305dd041d4a38604b
Alan Chen <t0239184ps@gmail.com>
2020-11-05 17:22:17 +0800

    Fix UTF8 problem

--- fgrep.py
+++ fgrep.py
@@ -5,7 +5,6 @@ import os
 import sys
 import time
 
-
 class Color:
     OK_BLUE = '\033[94m'
     OK_CYAN = '\033[96m'
@@ -21,7 +20,7 @@ class Color:
 
 
 '''
-   根据text文件数据类型判断是否是二进制文件
+   根据text文件数据类型判断是否是二进制文件UTF-8
    :param ff: 文件名（含路径）
    :return: True或False，返回是否是二进制文件
 '''
@@ -58,13 +57,14 @@ def search(filepath, pattern):
         if os.path.isdir(file_path):
             search(file_path, pattern)
         else:
+            # file_total_count += 1
             if is_binary_file(file_path):
                 continue
             file_process(file_path, list, pattern)
 
 
 def file_process(file_path, list, pattern):
-    with codecs.open(file_path, 'r', encoding='UTF-8') as file:
+    with codecs.open(file_path, 'r', encoding='UTF-8', errors='ignore') as file:
         i = 1
         is_first = True
         for line in file:
@@ -85,9 +85,9 @@ def file_process(file_path, list, pattern):
 def main():
     start_time = time.time()
     print('Number of arguments:', len(sys.argv), 'arguments.')
-    pattern = "UTF-8"
-    search(r'./', pattern)
+    pattern = "TODO"
+    path = '../../'
+    search(path, pattern)
     print('cost time:', '%.2f' % (time.time() - start_time), 'ms')
-
 
 main()
